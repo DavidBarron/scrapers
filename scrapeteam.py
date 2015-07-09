4ee4131e7ebfc110be35b225c4dfc68e5e0bf323
DavidBarron <david.barron91@gmail.com>
2015-07-09 14:48:57 -0500

    Add working files

---
+++ scrapeteam.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "http://www.dallascowboys.com/team/roster"
+r = requests.get(url)
+#print(type(r))
+#print(type(r.content))
+soup = BeautifulSoup(r.content)
+#links = soup.find_all("a")
+
+#for link in links
+#    print (link)
+
+name = "NA"
+team = "Dallas Cowboys"
+number = "NA"
+college = "NA"
+exp = "NA"
+height = "NA"
+weight = "NA"
+image = "NA"
+
+number_data = soup.find_all("div", {"class" : "field field--name-field-jersey-number field--type-number-integer field--label-hidden"})
+
+player_data = soup.find_all("div", {"class" : "field field--name-field-player field--type-entityreference field--label-hidden"})
+#print(type(player_data))
+for item in player_data :
+    print (item.contents[1])
+    soup2 = BeautifulSoup(item.contents[1])
